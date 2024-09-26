e67a45792be8c527014d8948f4347f05ce240a66
fanxing <fanxing1@jushuitan.com>
2024-09-26 23:12:56 +0800

    维度数据处理基本思路

---
+++ realtime-dim/src/main/java/com/gmall/realtime/dim/DimApp.java
@@ -0,0 +1,25 @@
+package com.gmall.realtime.dim;
+
+public class DimApp {
+    public static void main(String[] args) {
+//        1. 创建基本环境
+
+//        2. 检查点设置
+
+//        3. 从topic读取业务数据
+//        3.1 对数据进行etl处理
+
+//        4. 从MySQL读取配置表数据
+//        4.1 对数据进行etl处理
+
+//        5. 根据配置表数据到Hbase中进行建/删表操作
+
+//        6. todo 配置信息进行广播
+
+//        7. 主流业务数据和广播流进行连接
+
+//        8. 处理关联后的数据；process
+
+//        9. 将维度数据写到hbase中
+    }
+}
